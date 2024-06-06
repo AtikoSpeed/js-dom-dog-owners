c2214db50d294605fc57ad138ef681a492919ec8
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-06 21:39:55 +0300

    added is naughty text

--- src/index.js
+++ src/index.js
@@ -5,53 +5,53 @@
 const DOGS_LIST_UL = document.querySelector(".dogs-list");
 const DOGS_CARD_SECTION = document.querySelector(".main__dog-section");
 
-function createDoggerBio(DOG) {
-  const DOG_BIO_DIV = document.createElement("div");
-  DOG_BIO_DIV.classList.add("main__dog-section__desc");
-  const BIO_TITLE_H3 = document.createElement("h3");
-  BIO_TITLE_H3.textContent = "Bio";
-  const BIO_TEXT_P = document.createElement("p");
-  BIO_TEXT_P.textContent = DOG.bio;
-  DOG_BIO_DIV.appendChild(BIO_TITLE_H3);
-  DOG_BIO_DIV.appendChild(BIO_TEXT_P);
-  return DOG_BIO_DIV;
+function createDoggerBio(dog) {
+  const dogBioDiv = document.createElement("div");
+  dogBioDiv.classList.add("main__dog-section__desc");
+  const bioTitleH3 = document.createElement("h3");
+  bioTitleH3.textContent = "Bio";
+  const bioTextP = document.createElement("p");
+  bioTextP.textContent = dog.bio;
+  dogBioDiv.appendChild(bioTitleH3);
+  dogBioDiv.appendChild(bioTextP);
+  return dogBioDiv;
 }
 
-function createNaughtyButton(DOG) {
-  const IS_NAUGHTY_EM = document.createElement("em");
-  IS_NAUGHTY_EM.textContent = "Is naughty?";
-  const IS_NAUGHTY_BOOL_P = document.createElement("p");
-  if (DOG.isGoodDog == true) {
-    IS_NAUGHTY_BOOL_P.textContent = " No!";
+function createNaughtyText(dog) {
+  const isNaughtyP = document.createElement("p");
+  if (dog.isGoodDog == true) {
+    isNaughtyP.innerHTML = "<em>Is naughty?</em> No!";
   } else {
-    IS_NAUGHTY_BOOL_P.textContent = " Yes!";
+    isNaughtyP.innerHTML = "<em>Is naughty?</em> Yes!";
   }
+  return isNaughtyP;
 }
 
-function createDoggerCard(DOG) {
-  const DOG_NAME_H2 = document.createElement("h2");
+function createDoggerCard(dog) {
+  const dogNameH2 = document.createElement("h2");
   DOGS_CARD_SECTION.innerHTML = "";
-  DOG_NAME_H2.textContent = DOG.name;
-  const DOG_IMG = document.createElement("img");
-  DOG_IMG.src = DOG.image;
-  const DOG_BIO = document.createElement("img");
-  DOG_BIO_DIV = createDoggerBio(DOG);
-  DOGS_CARD_SECTION.appendChild(DOG_NAME_H2);
-  DOGS_CARD_SECTION.appendChild(DOG_IMG);
-  DOGS_CARD_SECTION.appendChild(DOG_BIO_DIV);
+  dogNameH2.textContent = dog.name;
+  const dogPic = document.createElement("img");
+  dogPic.src = dog.image;
+  dogBioDiv = createDoggerBio(dog);
+  dogIsNaughty = createNaughtyText(dog);
+  DOGS_CARD_SECTION.appendChild(dogNameH2);
+  DOGS_CARD_SECTION.appendChild(dogPic);
+  DOGS_CARD_SECTION.appendChild(dogBioDiv);
+  DOGS_CARD_SECTION.appendChild(dogIsNaughty);
 }
 
-function createDoggerButton(DOG) {
-  const DOG_BUTTON_LI = document.createElement("li");
-  DOG_BUTTON_LI.classList.add("dogs-list__button");
-  DOG_BUTTON_LI.textContent = DOG.name;
-  DOGS_LIST_UL.appendChild(DOG_BUTTON_LI);
-  DOG_BUTTON_LI.addEventListener("click", function () {
-    createDoggerCard(DOG);
+function createDoggerButton(dog) {
+  const dogButtonLi = document.createElement("li");
+  dogButtonLi.classList.add("dogs-list__button");
+  dogButtonLi.textContent = dog.name;
+  DOGS_LIST_UL.appendChild(dogButtonLi);
+  dogButtonLi.addEventListener("click", function () {
+    createDoggerCard(dog);
   });
 }
 
 for (let i = 0; i < data.length; i++) {
-  const DOG = data[i];
-  createDoggerButton(DOG);
+  const dog = data[i];
+  createDoggerButton(dog);
 }
