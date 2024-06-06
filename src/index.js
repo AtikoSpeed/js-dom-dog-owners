54efc8da727a4f796beccc3b09b1a6566bb1056d
Atakan Temizkan <atikotemizkan@gmail.com>
2024-06-06 19:31:06 +0300

    finished part 1

--- src/index.js
+++ src/index.js
@@ -5,14 +5,30 @@
 const DOGS_LIST_UL = document.querySelector(".dogs-list");
 const DOGS_CARD_SECTION = document.querySelector(".main__dog-section");
 
+function createDoggerBio(DOG) {
+  const DOG_BIO_DIV = document.createElement("div");
+  DOG_BIO_DIV.classList.add("main__dog-section__desc");
+  const BIO_TITLE_H3 = document.createElement("h3");
+  BIO_TITLE_H3.textContent = "Bio";
+  const BIO_TEXT_P = document.createElement("p");
+  BIO_TEXT_P.textContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+  DOG_BIO_DIV.appendChild(BIO_TITLE_H3);
+  DOG_BIO_DIV.appendChild(BIO_TEXT_P);
+  return DOG_BIO_DIV;
+}
+
 function createDoggerCard(DOG) {
   const DOG_NAME_H2 = document.createElement("h2");
   DOGS_CARD_SECTION.innerHTML = "";
   DOG_NAME_H2.textContent = DOG.name;
   const DOG_IMG = document.createElement("img");
   DOG_IMG.src = DOG.image;
+  const DOG_BIO = document.createElement("img");
+  DOG_BIO_DIV = createDoggerBio(DOG);
   DOGS_CARD_SECTION.appendChild(DOG_NAME_H2);
   DOGS_CARD_SECTION.appendChild(DOG_IMG);
+  DOGS_CARD_SECTION.appendChild(DOG_BIO_DIV);
 }
 
 function createDoggerButton(DOG) {
